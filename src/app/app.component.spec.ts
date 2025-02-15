7a9eea9f7c61fc35b4e512f6c5fa8a87006265e3
Marcello <hey@marcello.gg>
2025-02-15 13:49:15 -0500

    feature: migrate to Angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'fwdr2' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fwdr2');
+    expect(app.title).toEqual("fwdr2");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, fwdr2');
+    expect(compiled.querySelector("h1")?.textContent).toContain("Hello, fwdr2");
   });
 });
---
+++ src/app/enums/variants.enum.ts
@@ -0,0 +1,7 @@
+export enum Variant {
+  Success = "success",
+  Failure = "failure",
+  Warning = "warning",
+  Info = "info",
+  Default = "default",
+}
