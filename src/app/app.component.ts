682bdea2f9d828292719a19314b0e281e536fa1d
Marcello <hey@marcello.gg>
2025-02-15 14:47:24 -0500

    feature: demo in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { HeroHeaderComponent } from "./pages/homepage/hero-header/hero-header.component";
 import { FeatureCardComponent } from "./pages/homepage/feature-card/feature-card.component";
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: "app-root",
-  imports: [HeroHeaderComponent, FeatureCardComponent],
+  imports: [HeroHeaderComponent, FeatureCardComponent, NgOptimizedImage],
   templateUrl: "./app.component.html",
 })
 export class AppComponent {}
--- src/app/pages/homepage/hero-header/hero-header.component.ts
+++ src/app/pages/homepage/hero-header/hero-header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
 import { BadgeComponent } from "../../../components/badge/badge.component";
+import { DemoComponent } from "../../../components/demo/demo.component";
 
 @Component({
   selector: "app-hero-header",
-  imports: [BadgeComponent],
+  imports: [BadgeComponent, DemoComponent],
   template: `
     <header class="flex flex-col gap-4 container text-center">
       <h1 class="text-4xl flex gap-2 items-center justify-center">
@@ -18,6 +19,7 @@ import { BadgeComponent } from "../../../components/badge/badge.component";
         class="flex flex-col gap-4 bg-primary-300 border border-primary-200 py-8"
       >
         <h2 class="text-2xl">Try it Yourself</h2>
+        <app-demo />
       </div>
     </header>
   `,
