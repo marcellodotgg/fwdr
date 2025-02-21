abe9bc482dbada1d5c33f339e365214b40e0d059
Marcello <hey@marcello.gg>
2025-02-21 01:23:55 -0500

    feature: make the dashboard more polished

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { AsyncPipe } from "@angular/common";
+import { ContactUsComponent } from "../../components/contact-us/contact-us.component";
 
 @Component({
   selector: "app-dashboard",
-  imports: [],
+  imports: [AsyncPipe, ContactUsComponent],
   templateUrl: "./dashboard.component.html",
   styleUrl: "./dashboard.component.scss",
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  constructor(protected readonly auth: AuthService) {}
+}
